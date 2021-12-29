29f74671c3825ca7c04151cdaf1d5ed0ebf95a41
Sqh3rd <moitz417@gmail.com>
2021-12-29 21:35:05 +0100

    Yaba... Daba... DOO

---
+++ file_renamer.py
@@ -0,0 +1,29 @@
+import os
+
+for dir in os.listdir('./input_images'):
+
+    DIR = dir
+    print(dir)
+    if not dir == '3 - 9er Balken':
+        continue
+    FILES = []
+    for f in os.listdir(f'./input_images/{DIR}'):
+        if not f[:2] in FILES:
+            FILES.append(f[:2])
+    FILETYPE = '.jpg'
+
+    PATH = f'./input_images/{DIR}/'
+
+    for a, file in enumerate(FILES):
+        if file[-1] != '_':
+            file += '_'
+        path = f'{PATH}{file}'
+        
+        print(file)
+
+        for i in range(1,101):
+            number = f'{i}'
+            try:
+                os.rename(f'{path}{number}{FILETYPE}', f'{PATH}{a%70}_{i}{FILETYPE}')
+            except:
+                continue
--- simple_feature_extraction.py
+++ simple_feature_extraction.py
@@ -2,76 +2,42 @@ import cv2
 from time import sleep, time
 from matrix import Matrix, kernel_multiplicate, pooling, Flags
 import numpy as np
+from perceptron import Multilayer_Perceptron, Activation_Functions, Cost_Functions
 
-get_image_from_vid = False
-PATH='./input_images/cat_image_3.jpeg'
+min_diff_val = 1
 
-if get_image_from_vid:
+number_of_images = 10
+
+get_image_from_vid = True
+if not get_image_from_vid:
+    PATH='./input_images/p1/'
+    FILETYPE = '.jpg'
+
+    file_paths = []
+    for i in range(number_of_images):
+        file_paths.append(f'{PATH}{i}{FILETYPE}')
+
+    file_index = 0
+else:
     vid = cv2.VideoCapture(0)
 
-kernel = Matrix(
-    values=[[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]])
+kernel = Matrix(values=[[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 0], [0, 1, 0, 1]])
 filters = [
-    #100
-    #010
-    #001
     Matrix(values=[[1, -1, -1], [-1, 1, -1], [-1, -1, 1]]),
-    #010
-    #010
-    #010
     Matrix(values=[[-1, 1, -1], [-1, 1, -1], [-1, 1, -1]]),
-    #101
-    #010
-    #101
     Matrix(values=[[1, -1, 1], [-1, 1, -1], [1, -1, 1]]),
-    #001
-    #010
-    #100
     Matrix(values=[[-1, -1, 1], [-1, 1, -1], [1, -1, -1]]),
-    #010
-    #100
-    #010
     Matrix(values=[[-1, 1, -1], [1, -1, -1], [-1, 1, -1]]),
-    #010
-    #001
-    #010
     Matrix(values=[[-1, 1, -1], [-1, -1, 1], [-1, 1, -1]]),
-    #001
-    #010
-    #010
     Matrix(values=[[-1, -1, 1], [-1, 1, -1], [-1, 1, -1]]),
-    #100
-    #010
-    #010
     Matrix(values=[[1, -1, -1], [-1, 1, -1], [-1, 1, -1]]),
-    #010
-    #010
-    #100
     Matrix(values=[[-1, 1, -1], [-1, 1, -1], [1, -1, -1]]),
-    #010
-    #010
-    #001
     Matrix(values=[[-1, 1, -1], [-1, 1, -1], [-1, -1, 1]]),
-    #000
-    #001
-    #110
     Matrix(values=[[-1, -1, -1], [-1, -1, 1], [1, 1, -1]]),
-    #100
-    #011
-    #000
     Matrix(values=[[1, -1, -1], [-1, 1, 1], [-1, -1, -1]]),
-    #000
-    #110
-    #001
     Matrix(values=[[-1, -1, -1], [1, 1, -1], [-1, -1, 1]]),
-    #000
-    #011
-    #100
     Matrix(values=[[-1, -1, -1], [-1, 1, 1], [1, -1, -1]]),
-    #000
-    #111
-    #000
-    Matrix(values=[[-1, -1, -1], [1, 1, 1], [-1, -1, -1]]),
+    Matrix(values=[[-1, -1, -1], [1, 1, 1], [-1, -1, -1]])
 ]
 
 is_done = False
@@ -80,18 +46,16 @@ while not is_done:
     if get_image_from_vid:
         check, frame = vid.read()
     else:
-        frame = cv2.imread(PATH, 0)
+        frame = cv2.imread(file_paths[file_index], 0)
+        file_index += 1
 
     start = time()
-
-    print(frame.shape)
 
     if len(frame.shape) == 3:
         frame = [Matrix(values = [[frame[a][b][i] for b in range(frame.shape[1])] for a in range(frame.shape[0])]) for i in range(frame.shape[2])]
     else:
         frame = [Matrix(values=frame)]
 
-    print(repr(frame))
     for f in frame:
         if len(f.values) % 2 != 0:
             f.insert_line(0)
@@ -106,10 +70,8 @@ while not is_done:
         if len(k.values[0]) % 2 != 0:
             k.insert_column(0)
 
-    old_image = [[[]]]
-
-    cv2.imwrite("./images/old_image.jpg", np.array([[[f.values[a][b] for f in frame] for b in range(len(frame[0].values[a]))] for a in range(len(frame[0].values))]))
-    cv2.imwrite("./images/new_image.jpg", np.array([[[k.values[a][b] for k in kernel_multiplicated_image] for b in range(len(kernel_multiplicated_image[0].values[a]))] for a in range(len(kernel_multiplicated_image[0].values))]))
+    # cv2.imwrite("./images/old_image.jpg", np.array([[[f.values[a][b] for f in frame] for b in range(len(frame[0].values[a]))] for a in range(len(frame[0].values))]))
+    # cv2.imwrite("./images/new_image.jpg", np.array([[[k.values[a][b] for k in kernel_multiplicated_image] for b in range(len(kernel_multiplicated_image[0].values[a]))] for a in range(len(kernel_multiplicated_image[0].values))]))
 
     difference_frame = [kernel_multiplicate(first_matrix=Matrix(values=[[1,1,1],[1,1,1],[1,1,1]]), second_matrix=k, stride_length=1, crop_to_val=255, get_average=True, get_difference=True, keep_size=True) for k in kernel_multiplicated_image]
 
@@ -118,9 +80,16 @@ while not is_done:
     for diff in difference_frame:
         sum_matrix += diff
 
-    difference_frame = sum_matrix
+    sum_matrix.crop_to_value(255)
 
-    difference_frame.crop_to_value(255)
+    a = sum_matrix.count_values()
+
+    print([b for b in a if b[1] < 10])
+
+    sum_matrix.replace_multiple([i for i in range(min_diff_val)], -1)
+    sum_matrix.replace_multiple([i for i in range(min_diff_val, 251)], 1)
+
+    difference_frame = sum_matrix
 
     newer_frame = np.array(difference_frame.values)
 
@@ -128,13 +97,17 @@ while not is_done:
 
     new_images_filtered = []
 
-    i = 0
-    for filter in filters:
+    results = []
+
+    results = []
+    for i, filter in enumerate(filters):
+        s = time()
         new_image_filtered = kernel_multiplicate(first_matrix=filter, second_matrix=difference_frame, stride_length=1, crop_to_val=255, get_average=True, keep_size=True)
         new_image_filtered = pooling(new_image_filtered, (2,2), Flags.MAX_POOLING)
         new_images_filtered.append(new_image_filtered.values)
-        cv2.imwrite(f"./images/filtered_images/filter_{i}.jpg", np.array(new_image_filtered.values))
-        i += 1
+        e = time()
+        print(f'/\-Filter {i}\n--|{e-s}s\n{int((e-s)//60)}m {int((e-s)%60)}s\n')
+        cv2.imwrite(f"{PATH}/filters_{file_index}/{i}.jpg", np.array(new_image_filtered.values))
         
     is_done = True
 
